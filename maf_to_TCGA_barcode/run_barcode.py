e0bcc1d2c4cc326bda9f04e5f089ee54e6d2757f
LAPBIRD\bongo_000 <jeaton@mitre.org>
2017-04-09 19:44:46 -0400

    added run_barcode.py script to get comma separated list of TCGA barcodes from Broad Institute MAF file input

---
+++ maf_to_TCGA_barcode/run_barcode.py
@@ -0,0 +1,68 @@
+#     file: run_TCGA_barcode.py
+#   author: Jesse Eaton
+#  created: April 9, 2017
+# modified: April 9, 2017
+#  purpose: uses the Broad Institute's MAF sorted sum of all Fisher correlation values
+#             for each sample and prints the sample barcodes to standard output
+
+
+# # # # # # # # # # #
+#   I M P O R T S   #
+# # # # # # # # # # #
+
+import sys      # for command line arguments
+import os       # for manipulating files and folders
+import argparse # for command line arguments
+
+
+# # # # # # # # # # # # #
+#   C O N S T A N T S   #
+# # # # # # # # # # # # #
+
+TCGA_PREFIX = 'TCGA' # prefix of string to identify a TCGA barcode in input file
+DELIMINATOR = ','    # character used to separate each barcode in output
+
+
+# # # # # # # # # # # # #
+#   F U N C T I O N S   #
+# # # # # # # # # # # # #
+
+def main(argv):
+	args = get_args(argv)
+	barcodes = get_barcodes(args['input_file'])
+	print DELIMINATOR.join(barcodes)
+
+#  input: fname (string) name of input file (should have .txt extension)
+# output: barcodes (list of strings) TCGA barcodes for each sample listed in input file
+def get_barcodes(file):
+	barcodes = []
+	for line in file:
+		# split by tabs \t, get first column [0], and remove quotes using translate
+		first_col_val = line.split('\t')[0].translate(None, '\"')
+		if TCGA_PREFIX in first_col_val:
+			barcodes.append(first_col_val)
+	return barcodes
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+#   C O M M A N D   L I N E   A R G U M E N T   F U N C T I O N S   #
+# # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # # #
+
+def get_args(argv):
+	parser = argparse.ArgumentParser(prog = 'run_TCGA_barcode.py', description = "uses the Broad Institute's MAF sorted sum of all Fisher correlation values for each sample and prints the sample barcodes to standard output")
+	parser.add_argument('input_file', help = 'input .txt file', type = lambda x: is_valid_file(parser, x))
+	return vars(parser.parse_args(argv))
+
+def is_valid_file(parser, arg):
+	if not os.path.exists(arg):
+		parser.error('The file \"' + str(arg) + '\" could not be found.')
+	else:
+		return open(arg, 'r')
+
+
+# # # # # # # # # # # # # # # # # # # # # # # # #
+#   C A L L   T O   M A I N   F U N C T I O N   #
+# # # # # # # # # # # # # # # # # # # # # # # # #
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
